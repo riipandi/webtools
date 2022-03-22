1d68a57e61e9d359892ae6f53dcc7c8e63207519
Aris Ripandi <ar.is@outlook.com>
2022-03-23 02:37:25 +0700

    customize themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,10 @@
-import { createTheme, NextUIProvider } from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { useEffect } from 'react'
 
 import { initSplitBee } from '@/libraries/splitbee'
-
-const lightTheme = createTheme({
-  type: 'light',
-  theme: {}
-})
-
-const darkTheme = createTheme({
-  type: 'dark',
-  theme: {}
-})
+import { darkTheme, lightTheme } from '@/libraries/themes'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
