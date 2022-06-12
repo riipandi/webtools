1a26a552c8f2531a6eaeef7d1c21f9cf6e1d7044
Aris Ripandi <riipandi@gmail.com>
2022-06-13 02:20:02 +0700

    fix ip address detection

--- src/pages/api/ip-address.ts
+++ src/pages/api/ip-address.ts
@@ -1,19 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import geoip from 'fast-geoip'
-import publicIp from 'public-ip'
-import os from 'os'
 
 import { throwError } from '@/libraries/helpers'
+import axios from 'axios'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'GET') return throwError(res, 500, `Method not allowed!`)
 
+  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
   const forwarded = req.headers['x-forwarded-for'] || req.headers['x-real-ip']
   const remoteIP = typeof forwarded === 'string' ? forwarded.split(/, /)[0] : req.socket.remoteAddress
   const isLocalhost = remoteIP === '::1' || remoteIP === '127.0.0.1'
 
-  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-  const ip = isLocalhost ? await publicIp.v4() : remoteIP
+  const { data: resp } = await axios.get(`https://api.myip.com`)
+  const ip = isLocalhost ? resp.ip : remoteIP
   const geo = await geoip.lookup(ip)
 
   return res.status(200).json({
