b2896a0d4ff434109a70cbd9b654afaefed5418d
Aris Ripandi <riipandi@gmail.com>
2022-06-13 02:05:42 +0700

    fix ip address detection

--- src/pages/api/ip-address.ts
+++ src/pages/api/ip-address.ts
@@ -1,17 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import geoip from 'fast-geoip'
 import publicIp from 'public-ip'
+import os from 'os'
 
 import { throwError } from '@/libraries/helpers'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'GET') return throwError(res, 500, `Method not allowed!`)
 
-  const ipV4 = req.socket.remoteAddress.replace(/^.*:/, '')
-  const isLocalhost = ipV4 === '1' ? true : false
+  const forwarded = req.headers['x-forwarded-for']
+  const remoteIP = typeof forwarded === 'string' ? forwarded.split(/, /)[0] : req.socket.remoteAddress
+  const isLocalhost = remoteIP === '::1' || remoteIP === '127.0.0.1'
 
   const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-  const ip = isLocalhost ? await publicIp.v4() : ipV4
+  const ip = isLocalhost ? await publicIp.v4() : remoteIP
   const geo = await geoip.lookup(ip)
 
   return res.status(200).json({
