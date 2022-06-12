77849e756f077479324636264d4e3db2d76a648f
Aris Ripandi <riipandi@gmail.com>
2022-06-13 02:07:46 +0700

    fix ip address detection

--- src/pages/api/ip-address.ts
+++ src/pages/api/ip-address.ts
@@ -8,7 +8,7 @@ import { throwError } from '@/libraries/helpers'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'GET') return throwError(res, 500, `Method not allowed!`)
 
-  const forwarded = req.headers['x-forwarded-for']
+  const forwarded = req.headers['x-forwarded-for'] || req.headers['x-real-ip']
   const remoteIP = typeof forwarded === 'string' ? forwarded.split(/, /)[0] : req.socket.remoteAddress
   const isLocalhost = remoteIP === '::1' || remoteIP === '127.0.0.1'
 
